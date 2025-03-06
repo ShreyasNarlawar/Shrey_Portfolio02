53a3aeddbecb6a2de178c2a7dbee63291e55bf83
ShreyasNarlawar <narlawarshreyas@gmail.com>
2025-03-06 15:55:56 +0530

    final commit

---
+++ portfolio/send_mail.php
@@ -0,0 +1,27 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $message = htmlspecialchars($_POST["message"]);
+    
+    $to = "narlawarshreyas@gmail.com"; // Your Email
+    $subject = "New Contact Form Submission from $name";
+    $headers = "From: $email" . "\r\n" .
+               "Reply-To: $email" . "\r\n" .
+               "Content-Type: text/plain; charset=UTF-8";
+    
+    $body = "You have received a new message from your website contact form:\n\n";
+    $body .= "Name: $name\n";
+    $body .= "Email: $email\n";
+    $body .= "Message:\n$message\n";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "<script>alert('Message sent successfully!'); window.location.href='contact.html';</script>";
+    } else {
+        echo "<script>alert('Error sending message. Please try again.'); window.location.href='contact.html';</script>";
+    }
+} else {
+    header("Location: contact.html");
+    exit();
+}
+?>
